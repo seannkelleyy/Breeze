743753d67b96c4e641b96bc9d306dd7ff1638d9c
seankelley <kelley.1102@buckeyemail.osu.edu>
2024-09-15 10:53:50 -0400

    Updates api url

--- Breeze.Web/src/config/environment/useEnvironmentVariables.ts
+++ Breeze.Web/src/config/environment/useEnvironmentVariables.ts
@@ -44,7 +44,7 @@ export const useEnvironmentVariables = (): EnvironmentVariables => {
             : windowConfig[envVar]
 
     const baseLocalApi = getValue('BASE_LOCAL_API', 'https://localhost:7284')
-    const baseHostedApi = getValue('BASE_HOSTED_API', 'https://www.breeze-apiapp.azurewebsites.net')
+    const baseHostedApi = getValue('BASE_HOSTED_API', 'https://www.breeze-api.azurewebsites.net')
     const baseLocalUrl = getValue('BASE_LOCAL_URL', 'http://localhost:5173/')
     const baseHostedUrl = getValue('BASE_HOSTED_URL', 'https://www.breeze.seannkelleyy/')
     const baseLocalLoginUrl = getValue('BASE_LOCAL_LOGIN_URL', 'http://localhost:5173/login')
