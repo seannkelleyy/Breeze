7fcfa8c62b6dbd058b39e995f586b174658df987
seankelley <kelley.1102@buckeyemail.osu.edu>
2024-09-15 10:47:45 -0400

    Updates Config to host

--- Breeze.Web/src/config/environment/envConfig.js
+++ Breeze.Web/src/config/environment/envConfig.js
@@ -1,10 +1,10 @@
 window.ENV_CONFIG = {
     BASE_LOCAL_URL: 'http://localhost:5173/',
-    BASE_HOSTED_URL: 'https://breeze.seannkelleyy.com/',
+    BASE_HOSTED_URL: 'https://www.breeze.seannkelleyy.com/',
     BASE_LOCAL_LOGIN_URL: 'http://localhost:5173/login',
-    BASE_HOSTED_LOGIN_URL: 'https://breeze.seannkelleyy.com/login',
+    BASE_HOSTED_LOGIN_URL: 'https://wwww.breeze.seannkelleyy.com/login',
     BASE_LOCAL_API: 'https://localhost:7284',
-    BASE_HOSTED_API: 'https://breeze-apiapp.azurewebsites.net',
+    BASE_HOSTED_API: 'https://www.breeze-apiapp.azurewebsites.net',
     APP_VERSION: '1.1.0',
     AUTH_CLIENT_ID: 'Osw8qGrmXjQe3kYYcL0ca3FdUqB9LeVd',
     AUTH_DOMAIN: 'https://dev-r15wsyccxyjfwrqm.us.auth0.com/',
--- Breeze.Web/src/config/environment/useEnvironmentVariables.ts
+++ Breeze.Web/src/config/environment/useEnvironmentVariables.ts
@@ -44,11 +44,11 @@ export const useEnvironmentVariables = (): EnvironmentVariables => {
             : windowConfig[envVar]
 
     const baseLocalApi = getValue('BASE_LOCAL_API', 'https://localhost:7284')
-    const baseHostedApi = getValue('BASE_HOSTED_API', 'https://breeze-apiapp.azurewebsites.net')
+    const baseHostedApi = getValue('BASE_HOSTED_API', 'https://www.breeze-apiapp.azurewebsites.net')
     const baseLocalUrl = getValue('BASE_LOCAL_URL', 'http://localhost:5173/')
-    const baseHostedUrl = getValue('BASE_HOSTED_URL', 'https://breeze.seannkelleyy/')
+    const baseHostedUrl = getValue('BASE_HOSTED_URL', 'https://www.breeze.seannkelleyy/')
     const baseLocalLoginUrl = getValue('BASE_LOCAL_LOGIN_URL', 'http://localhost:5173/login')
-    const baseHostedLoginUrl = getValue('BASE_HOSTED_LOGIN_URL', 'https://breeze.seannkelleyy.com/login')
+    const baseHostedLoginUrl = getValue('BASE_HOSTED_LOGIN_URL', 'https://www.breeze.seannkelleyy.com/login')
     const appVersion = getValue('APP_VERSION', '1.0.0')
     const authClientId = getValue('VITE_AUTH_CLIENT_ID', 'Osw8qGrmXjQe3kYYcL0ca3FdUqB9LeVd')
     const authDomain = getValue('VITE_AUTH_DOMAIN', 'dev-r15wsyccxyjfwrqm.us.auth0.com')
