8bcad228eaaa4126401930c69c242800bf052304
seankelley <kelley.1102@buckeyemail.osu.edu>
2024-09-16 13:04:09 -0400

    Updates frontend environment variables to match correct api url

--- Breeze.Web/src/config/environment/envConfig.js
+++ Breeze.Web/src/config/environment/envConfig.js
@@ -4,7 +4,7 @@ window.ENV_CONFIG = {
     BASE_LOCAL_LOGIN_URL: 'http://localhost:5173/login',
     BASE_HOSTED_LOGIN_URL: 'https://wwww.breeze.seannkelleyy.com/login',
     BASE_LOCAL_API: 'https://localhost:7284',
-    BASE_HOSTED_API: 'https://www.breeze-api.azurewebsites.net',
+    BASE_HOSTED_API: 'https://breeze-api.azurewebsites.net',
     APP_VERSION: '1.1.0',
     AUTH_CLIENT_ID: 'Osw8qGrmXjQe3kYYcL0ca3FdUqB9LeVd',
     AUTH_DOMAIN: 'https://dev-r15wsyccxyjfwrqm.us.auth0.com/',
