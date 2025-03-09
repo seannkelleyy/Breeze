44e0fd504037f3b126ebeb464adcadcae97fd127
seankelley <kelley.1102@buckeyemail.osu.edu>
2025-03-09 17:48:27 -0400

    Feat (landing page): Adds button to go to dashboard from landing page.

--- Breeze.Web/src/sections/LandingPage.tsx
+++ Breeze.Web/src/sections/LandingPage.tsx
@@ -1,6 +1,11 @@
+import { useMsal } from '@azure/msal-react'
 import { AuthButton } from '../components/auth/AuthButton'
+import { Button } from '../components/ui/button'
+import { Link } from 'react-router-dom'
 
 export const LandingPage = () => {
+	const accounts = useMsal().instance.getAllAccounts()
+
 	return (
 		<section className='h-screen w-screen flex flex-col justify-center items-center'>
 			<div className='flex flex-col justify-center items-center gap-2 h-full'>
@@ -9,7 +14,16 @@ export const LandingPage = () => {
 				<p className='w-full text-right'>
 					to <span className='text-accent'>budget</span>.
 				</p>
-				<AuthButton />
+				{accounts.length > 0 ? (
+					<div className='flex flex-col gap-2'>
+						<p className='w-full text-center'>Welcome, {accounts[0].name}</p>
+						<Link to='/'>
+							<Button>Go to Dashboard</Button>
+						</Link>
+					</div>
+				) : (
+					<AuthButton />
+				)}
 			</div>
 		</section>
 	)
