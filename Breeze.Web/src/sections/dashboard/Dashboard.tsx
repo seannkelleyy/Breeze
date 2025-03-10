69963fd4df8cdcbe36b68a56cc73cf6adfc9cd3c
seankelley <kelley.1102@buckeyemail.osu.edu>
2025-03-10 09:30:15 -0400

    Upgrades ESbuild

--- Breeze.Web/src/sections/dashboard/Dashboard.tsx
+++ Breeze.Web/src/sections/dashboard/Dashboard.tsx
@@ -43,7 +43,7 @@ export const Dashboard = () => {
 	const budgetDifference = (budget?.monthlyIncome ?? 0) - (budget?.monthlyExpenses ?? 0)
 
 	return (
-		<Card className='absolute left-1/2 transform -translate-x-1/2 overflow-x-hidden flex flex-col gap-1 justify-start items-center p-4 mt-30'>
+		<Card className='text-center mt-[15vh] p-4 max-w-[750px] rounded-[.5rem] overflow-x-hidden flex flex-col gap-1 justify-start items-center '>
 			<div className='flex gap-4 mb-4'>
 				<Button
 					onClick={getPreviousBudget}
