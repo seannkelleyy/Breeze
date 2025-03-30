721b0381938fd6c44456d5658c4f18746e46318e
seankelley <kelley.1102@buckeyemail.osu.edu>
2025-03-30 15:29:38 -0400

    Feat (Auth): Changes auth depending on device type

--- Breeze.Web/src/components/auth/AuthButton.tsx
+++ Breeze.Web/src/components/auth/AuthButton.tsx
@@ -1,11 +1,24 @@
 import { useMsal, AuthenticatedTemplate, UnauthenticatedTemplate } from '@azure/msal-react'
 import { Button } from '../ui/button'
 import { loginRequest } from '../../config/auth/authConfig'
+import useDeviceDetection from '../../config/device/useDeviceDetection'
 
 export const AuthButton = () => {
 	const { instance } = useMsal()
+	const device = useDeviceDetection()
+	const isMobile = device === 'Mobile' || device === 'Tablet'
+
+	console.log('Device:', device)
+	console.log('isMobile:', isMobile)
 
 	const handleLogin = () => {
+		if (isMobile) {
+			instance.loginRedirect({
+				...loginRequest,
+				redirectUri: '/',
+			})
+			return
+		}
 		instance
 			.loginPopup({
 				...loginRequest,
@@ -15,8 +28,14 @@ export const AuthButton = () => {
 	}
 
 	const handleLogout = () => {
+		if (isMobile) {
+			instance.logoutRedirect({
+				postLogoutRedirectUri: '/login',
+			})
+			return
+		}
 		instance.logoutPopup({
-			postLogoutRedirectUri: '/',
+			postLogoutRedirectUri: '/login',
 		})
 		window.location.reload()
 	}
--- Breeze.Web/src/config/auth/authConfig.ts
+++ Breeze.Web/src/config/auth/authConfig.ts
@@ -6,7 +6,7 @@ export const msalConfig = {
 	},
 	cache: {
 		cacheLocation: 'localStorage',
-		storeAuthStateInCookie: true,
+		storeAuthStateInCookie: false,
 	},
 }
 
